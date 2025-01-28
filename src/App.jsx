765ca19ed5b8ad160c602f3c52ae1170d9162bee
ShanmathiVasudevan <24b1020@iitb.ac.in>
2025-01-28 15:33:57 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react';
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=2')
+    .then((response) => response.json())
+    .then((data) => {console.log(data); setPosts(data);})
+    .catch((err) => {console.log(err.message);});
+  }, []);
+
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const addPosts = async(title,body) => {
+    await fetch('https://jsonplaceholder.typicode.com/posts',{method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: Math.random().toString(36).slice(2),
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF -8',
+      },
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      setPosts((posts) => [data, ...posts]);
+      setTitle('');
+      setBody('');
+    })
+    .catch((err) => {console.log(err.message)});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addPosts(title,body);
+  }
+
+  const [selection, setSelection] = useState('');
+  useEffect(() => {
+    document.addEventListener('selectionchange', () => {
+      setSelection(document?.getSelection()?.toString());
+    });
+  },[])
+  console.log(selection);
+
+  const [bold, setBold]=useState(false);
+  function isBold(arg){
+    let arg1=[arg];
+    arg1=arg1.map(arg => <span style={{fontWeight:bold?'bold':'normal'}}>{arg}</span>);
+    
+  }
+
+  return (
+    <>
+      <div className="create-post">
+        <button onClick={isBold(document.getSelection())}><b>B</b></button>
+        <button><i>I</i></button>
+        <button><u>U</u></button>
+        <form onSubmit={handleSubmit}>
+          <input type='text' placeholder="Enter title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
+          <br></br>
+          <textarea contentEditable placeholder="Type the body of your post..." name="" id="" value={body} onChange={(e) => setBody(e.target.value)}/>
+          <br></br>
+          <button type="submit">Add Post</button>
+        </form>
+      </div>
+      <div>
+        {posts.map((posts) => {
+          return(<div key={posts.id}>
+        <h2>{posts.title}</h2>
+        <p>{posts.body}</p>
+      </div>);
+    })}
+      
+      </div>
+    </>
+  )
+}
+
+export default App
