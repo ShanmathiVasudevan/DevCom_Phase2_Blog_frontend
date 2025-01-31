eaff71dd0b24c7601500caf2932fd95bb4ae98bd
unknown <krithiksaikishore@gmail.com>
2025-01-31 18:16:18 +0530

    deleted the folder

--- devcom-frontend/src/PreviewRow.jsx
+++
@@ -1,9 +0,0 @@
-import Preview from './Preview.jsx'
-function PreviewRow(){
-    return(
-        <div className="preview-row">
-            <Preview/>
-        </div>
-    );
-}
-export default PreviewRow
