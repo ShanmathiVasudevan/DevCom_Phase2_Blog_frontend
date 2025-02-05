f0802632da01af62e45a1d979667f64301d83436
ShanmathiVasudevan <24b1020@iitb.ac.in>
2025-02-05 18:14:10 +0530

    IMPROVED FILE SRTCTURE

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,13 @@
 import React, {useState, useEffect} from 'react';
-import './App.css'
-import ReactQuill from "react-quill";
-import EditorToolbar, { modules, formats } from "./EditorToolbar";
 import "react-quill/dist/quill.snow.css";
+import EditBlog from './Pages/EditBlog/EditBlog';
+
+
 function App() {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts?_limit=2')
-    .then((response) => response.json())
-    .then((data) => {console.log(data); setPosts(data);})
-    .catch((err) => {console.log(err.message);});
-  }, []);
-
-
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState({value:null});
-  const handleChange = value => {
-    setBody({ value });
-  };
-  const addPosts = async(title,body) => {
-    await fetch('https://jsonplaceholder.typicode.com/posts',{method: 'POST',
-      body: JSON.stringify({
-        title: title,
-        body: body,
-        userId: Math.random().toString(36).slice(2),
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF -8',
-      },
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      setPosts((posts) => [data, ...posts]);
-      setTitle('');
-      setBody('');
-    })
-    .catch((err) => {console.log(err.message)});
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    addPosts(title,body);
-  }
 
   return (
     <>
-    <div className="create-post">
-        <form onSubmit={handleSubmit}>
-          <div className="text-editor">
-                <EditorToolbar />
-                <input type='text' placeholder="Enter title" value={title} onChange={(e) => setTitle(e.target.value)}></input>
-                <ReactQuill
-                  theme="snow"
-                  value={body.value}
-                  onChange={handleChange}
-                  placeholder={'Enter body'}
-                  modules={modules}
-                  formats={formats}
-                />
-              </div>
-          <button type="submit">Add Post</button>
-          
-        </form>
-      </div>
-      <div>
-        {posts.map((posts) => {
-          return(<div key={posts.id}>
-        <h2>{posts.title}</h2>
-        <p>{posts.body}</p>
-      </div>);
-    })}
-      
-      </div>
+    <EditBlog/>
       
     </>
   )
